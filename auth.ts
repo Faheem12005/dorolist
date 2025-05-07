36ba66e53fb24444b249bcd5bc48b5a7b89ff52c
Mohammed <mohdfaheem.1205@gmail.com>
2025-05-07 22:38:23 +0530

    smaller fix

--- auth.ts
+++ auth.ts
@@ -1,11 +1,27 @@
 import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
 import { SupabaseAdapter } from "@auth/supabase-adapter"
- 
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [GitHub],
-  adapter: SupabaseAdapter({
-    url: process.env.SUPABASE_URL!,
-    secret: process.env.SUPABASE_SERVICE_ROLE_KEY!,
-  }),
+    providers: [GitHub],
+    pages: {
+        signIn: "/login"
+    },
+    adapter: SupabaseAdapter({
+        url: process.env.SUPABASE_URL!,
+        secret: process.env.SUPABASE_SERVICE_ROLE_KEY!,
+    }),
+    callbacks: {
+        authorized({ auth, request: { nextUrl } }) {
+            const isLoggedIn = !!auth?.user;
+            const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+            if (isOnDashboard) {
+                if (isLoggedIn) return true;
+                return false; 
+            } else if (isLoggedIn) {
+                return Response.redirect(new URL('/dashboard', nextUrl));
+            }
+            return true;
+        },
+    }
 })
