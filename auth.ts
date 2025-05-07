13588c3f76f5a4f38d15240e6d1ddac534bfd37d
Mohammed <mohdfaheem.1205@gmail.com>
2025-05-07 22:03:45 +0530

    small fix

--- auth.ts
+++ auth.ts
@@ -5,7 +5,7 @@ import { SupabaseAdapter } from "@auth/supabase-adapter"
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [GitHub],
   adapter: SupabaseAdapter({
-    url: process.env.SUPABASE_URL,
-    secret: process.env.SUPABASE_SERVICE_ROLE_KEY,
+    url: process.env.SUPABASE_URL!,
+    secret: process.env.SUPABASE_SERVICE_ROLE_KEY!,
   }),
 })
