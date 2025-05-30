c5946895894331cc114edca64b7e5ccd3bbb2bad
Mohammed <mohdfaheem.1205@gmail.com>
2025-05-30 19:36:28 +0530

    small fix

--- app/dashboard/[projectid]/edit/page.tsx
+++ app/dashboard/[projectid]/edit/page.tsx
@@ -3,10 +3,9 @@ import TaskHandler from "@/app/ui/edit/task-handler";
 export default async function Page({
   params,
 }: {
-  params: { projectid: string };
+  params: Promise<{ projectid: string }>;
 }) {
-  const { projectid } = params;
-
+  const { projectid } = await params;
   return (
     <div className="min-h-screen flex flex-col p-5 w-full">
       <TaskHandler projectId={projectid} />
